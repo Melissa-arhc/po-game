d5772ba4a2a3833455383229453a5075e94ddf2b
Melissa-arhc <138522891+Melissa-arhc@users.noreply.github.com>
2024-06-29 14:39:22 +0000

    Achtergrond instellen

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pgzrun
+import pygame
+
+# Window instellen
+screen_width = 600
+screen_height = 350
+
+# Achtergrond instellen
+background = pygame.image.load("images/achtergrond.jpeg")
+background = pygame.transform.scale(background, (screen_width, screen_height))
 
 
 def draw():
-    pass
+    screen.clear()
+    screen.blit(background, (0, 0))
 
 
 def update():
