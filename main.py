b9cea5eedb882405c9a4179e286ad56e7fd4dcbb
Melissa-arhc <138522891+Melissa-arhc@users.noreply.github.com>
2024-06-29 15:14:26 +0000

    Ruimteschip toegevoegd en laten reageren op pijltjes

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ import pgzrun
 import pygame
 
 # Window instellen
-screen_width = 600
-screen_height = 350
+WIDTH = 600
+HEIGHT = 350
+MARGIN = 10
 
 # Achtergrond instellen
 background = pygame.image.load("images/achtergrond.jpeg")
-background = pygame.transform.scale(background, (screen_width, screen_height))
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+# Ruimteschip instellen
+spaceship_image = pygame.image.load("images/ruimteschip.png")
+spaceship_image = pygame.transform.scale(spaceship_image, (80, 80))
+spaceship_image = pygame.transform.rotate(spaceship_image, -45)
+temp_file = "images/ruimteschip_rotated.png"
+pygame.image.save(spaceship_image, temp_file)
+
+spaceship = Actor("ruimteschip_rotated")
+spaceship.pos = WIDTH // 2, HEIGHT // 2
 
 
 def draw():
     screen.clear()
     screen.blit(background, (0, 0))
+    spaceship.draw()
 
 
 def update():
-    pass
+    if keyboard.up and spaceship.y > MARGIN:
+        spaceship.y -= 5
+    if keyboard.down and spaceship.y < HEIGHT - MARGIN:
+        spaceship.y += 5
 
 
 def game_over():
