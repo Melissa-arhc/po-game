a4273f1a6f6a0c1bd05b2fcdd2c611c0187b5d35
Melissa-arhc <138522891+Melissa-arhc@users.noreply.github.com>
2024-06-29 15:37:47 +0000

    Meteor toegevoegd (na een paar seconden) en laten bewegen van rechts naar links, en laten herhalen met een random y

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pgzrun
 import pygame
+import random
+import time
 
 # Window instellen
 WIDTH = 600
 HEIGHT = 350
 MARGIN = 10
+MARGIN_METEOR = 20
+DELAY_METEOR = 3
 
 # Achtergrond instellen
 background = pygame.image.load("images/achtergrond.jpeg")
@@ -20,18 +24,45 @@ pygame.image.save(spaceship_image, temp_file)
 spaceship = Actor("ruimteschip_rotated")
 spaceship.pos = WIDTH // 2, HEIGHT // 2
 
+# Meteor instellen
+meteor_image = pygame.image.load("images/meteor.png")
+meteor_image = pygame.transform.scale(meteor_image, (80, 80))
+meteor_image = pygame.transform.rotate(meteor_image, -45)
+temp_file = "images/meteor_small.png"
+pygame.image.save(meteor_image, temp_file)
+
+meteor = Actor("meteor_small")
+meteor.pos = WIDTH // 2, HEIGHT
+
+# Timer
+start_time = time.time()
+meteor_created = False
+
 
 def draw():
     screen.clear()
     screen.blit(background, (0, 0))
     spaceship.draw()
+    if meteor_created:
+        meteor.draw()
 
 
 def update():
+    global meteor_created
+    # Start de eerste meteor na een vertraging
+    if not meteor_created and time.time() - start_time > DELAY_METEOR:
+        meteor_created = True
+        meteor.pos = WIDTH + MARGIN_METEOR, random.randint(0, HEIGHT)
+
     if keyboard.up and spaceship.y > MARGIN:
         spaceship.y -= 5
     if keyboard.down and spaceship.y < HEIGHT - MARGIN:
         spaceship.y += 5
+    if meteor.x < 0:
+        meteor.x = WIDTH + MARGIN_METEOR
+        meteor.y = random.randint(0, HEIGHT)
+
+    meteor.x -= 5
 
 
 def game_over():
